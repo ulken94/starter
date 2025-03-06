3205c31b254d8c4115cfe79e2cc72f36c74bf29c
ulken94 <bestwook7@gmail.com>
2025-03-06 14:52:00 +0900

    Add python macros

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "@d", "$a<CR>__import__('pdb').set_trace()<ESC>:w<CR>")
+map("n", "@m", "$a<CR>if __name__ == \"__main__\":")
+
