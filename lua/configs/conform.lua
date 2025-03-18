5b7936bdef7f396d77060b62e7d7ababd019a68f
Haneol Kim <bestwook7@gmail.com>
2025-03-18 10:34:30 +0900

    Add isort

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,7 +1,7 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    python = { "black" },
+    python = { "black", "isort" },
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
