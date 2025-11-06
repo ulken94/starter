c74217094678df614e53ebd50305effddfea6a59
Haneol Kim <bestwook7@gmail.com>
2025-11-06 11:39:11 +0900

    Change copilot accpet suggestion to C-o

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -4,13 +4,11 @@ return {
     "github/copilot.vim",
     branch = "release",
     lazy = false,
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.api.nvim_set_keymap("i", "<C-o>", 'copilot#Accept("\\<CR>")', { silent = true, expr = true, replace_keycodes = false, })
+    end,
   },
-  -- {
-  --   "zbirenbaum/copilot-cmp",
-  --   dependencies = { "zbirenbaum/copilot.lua" },
-  --   config = function()
-  --     require("copilot_cmp").setup()
-  --   end,
-  -- }
 }
 
